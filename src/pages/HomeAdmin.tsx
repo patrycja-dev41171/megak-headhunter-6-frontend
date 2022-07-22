af1cf7358674ffa4dc4e9f5ec0b6d16df7b00987
Mateusz Umięcki <mateuszumiecki@gmail.com>
2022-07-22 18:09:58 +0200

    Prettier fix code

--- src/pages/HomeAdmin.tsx
+++ src/pages/HomeAdmin.tsx
@@ -1,38 +1,33 @@
-import React, {FormEvent, useState} from 'react';
+import React, { FormEvent, useState } from 'react';
 
 export const HomeAdmin = () => {
+  const [photo, setPhoto] = useState();
 
-    const [photo, setPhoto] = useState();
+  function changeHandler(event: FormEvent) {
+    setPhoto((event.target as any).files[0]);
+  }
 
+  function handleSubmission() {
+    if (photo) {
+      const formData = new FormData();
 
-    function changeHandler(event: FormEvent) {
-        setPhoto((event.target as any).files[0]);
+      formData.append('photo', photo);
 
+      fetch('http://localhost:8080/upload', {
+        method: 'POST',
+        body: formData,
+      });
     }
+  }
 
-    function handleSubmission() {
-
-        if (photo) {
-
-            const formData = new FormData();
-
-            formData.append('photo', photo);
-
-            fetch('http://localhost:8080/upload',
-                {
-                    method: 'POST',
-                    body: formData,
-                }
-            );
-        }
-    }
-
-
-    return <div>
-
-        <input type="file" name="foo" onChange={changeHandler}/>
-        <button onClick={handleSubmission}>Wyślij</button>
-
+  return (
+    <div>
+      <input
+        type="file"
+        name="foo"
+        onChange={changeHandler}
+      />
+      <button onClick={handleSubmission}>Wyślij</button>
     </div>
-
+  );
 };
