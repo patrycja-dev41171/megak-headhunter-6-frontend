442c4d0c970d6f246f8b85f4ea8f9e29700273bb
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-07-23 14:42:36 +0200

    Added react-router-dom and configured routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import './App.css';
-import { HomeAdmin } from './pages/HomeAdmin';
+import { LoginView } from './pages/LoginViews/LoginView';
+import {Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <HomeAdmin></HomeAdmin>
+        <Routes>
+            <Route path="/registration" element={<LoginView/>}/>
+        </Routes>
     </>
-  );
-}
+)}
 
 export default App;
