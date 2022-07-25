8968c82168f54cb32f8345b23a57a50a9a7d87e2
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-07-25 21:42:41 +0200

    Configured routing.

--- src/pages/RegisterView/RegisterView.tsx
+++ src/pages/RegisterView/RegisterView.tsx
@@ -4,6 +4,8 @@ export const RegisterView = () => {
   return (
     <>
       <h1>Register View</h1>
+      <p>Strona rejestracji student/hr z linku w email. Formularz (hasło/hasło)</p>
+      <p>Dostęp: Niezalogowany użytkownik który ma link.</p>
     </>
   );
 };
