9db041906e6c38e3f5639eb79b207a8df331b4b1
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-07-25 21:44:15 +0200

    Added views structure and simple views components.

---
+++ src/pages/ForgotPasswordView/ForgotPasswordView.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const ForgotPasswordView = () => {
+  return (
+    <>
+      <h1>Forgot Password View</h1>
+      <p>Strona odzyskania hasła - formularz (email)</p>
+      <p>Dostęp: Dostępna dla niezalogowanego użytkownika </p>
+    </>
+  );
+};
---
+++ src/pages/StudentView/StudentView.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const StudentView = () => {
+  return (
+    <>
+      <h1>Student View</h1>
+      <p>
+        Widok po rejestracji/zalogwowaniu się kursanta. Widok z profilem użytkownika. Możliwość edycji swoich danych lub dodania nowych
+        przez użytkownika oraz przycisk wylogowania. (edycja danych w tabeli student)
+      </p>
+      <p>Dostęp: Zalogowany student</p>
+    </>
+  );
+};
