08c734cb04ed4d41806b5a4e13ccb62c464442b0
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-07-30 23:34:57 +0200

    Added header component to homeAdminView and corrected style.

--- src/pages/HomeAdmin/HomeAdmin.tsx
+++ src/pages/HomeAdmin/HomeAdmin.tsx
@@ -2,27 +2,19 @@ import React from 'react';
 
 import './HomeAdmin.css';
 import { StudentImport } from '../../components/StudentImport/StudentImport';
-import { AddHR } from '../../components/AddHR/AddHR';
-import { LogoutBtn } from '../../components/LogoutBtn/LogoutBtn';
+import {AddHR} from '../../components/AddHR/AddHR';
+import {Header} from '../../components/Header/Header';
 
 export const HomeAdmin = () => {
   return (
-    <div className="home-admin-container">
-      <header className="home-admin_header">
-        <img
-          className="home-admin_img"
-          src="/logo-megak.webp"
-          alt="Logo Mega K"
-        />
-        <p className="home-admin_logout">
-          <LogoutBtn />
-        </p>
-      </header>
-
-      <main className="home-admin_main">
-        <StudentImport />
-        <AddHR />
-      </main>
-    </div>
+      <>
+          <div className="home-admin-container">
+              <Header/>
+              <main className="home-admin_main">
+                  <StudentImport/>
+                  <AddHR/>
+              </main>
+          </div>
+      </>
   );
 };
