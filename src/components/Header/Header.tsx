548dd60ca74cd852a5bab3422b05e792218cab2b
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-07-31 21:21:32 +0200

    Added props for clean code in header component.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,31 +2,22 @@ import React from 'react';
 
 import './Header.css';
 import {HeaderMenu} from '../HeaderMenu/HeaderMenu';
+import {MicroLogoMegaK} from '../../common/MicroLogoMegaK/MicroLogoMegaK';
+import {UserPhoto} from '../../common/UserPhoto/UserPhoto';
 
 export const Header = () => {
   return (
       <>
         <div className="main-header">
           <div className="logo-background">
-            <img
-                src="../../logo-megak.webp"
-                alt="Logo MegaK"
-                className="logo"
-            />
+            <MicroLogoMegaK/>
           </div>
           <div className="user-info">
-            <div className="photo-space">
-              <img
-                  src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=826&t=st=1659204468~exp=1659205068~hmac=936a7aaf9d679d433eb81584d87be66f60eb01c0498e1abcabfe6de62a060c69"
-                  alt="Zdjęcie użytkownika"
-                  className="photo"
-              />
+            <UserPhoto className="photo-space"/>
+            <div className="user-name">
+              <p>Mateusz Kowalski</p>
             </div>
-            <div className="user-name"></div>
-            <p>Tester Testowy Do Testów</p>
-            <div className="options-btn">
-              <HeaderMenu/>
-            </div>
+            <HeaderMenu className="options-btn"/>
           </div>
         </div>
       </>
--- src/components/HeaderMenu/HeaderMenu.tsx
+++ src/components/HeaderMenu/HeaderMenu.tsx
@@ -1,108 +1,94 @@
-import React, {SyntheticEvent} from 'react';
-import {MenuItem} from '@mui/material';
+import React, {MouseEventHandler, SyntheticEvent} from 'react';
 import {styled} from '@mui/material/styles';
 import Menu, {MenuProps} from '@mui/material/Menu';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import {useNavigate} from 'react-router-dom';
 
 import './HeaderMenu.css';
+import {MenuBtn} from '../../common/MenuBtn/MenuBtn';
+import {ShowMenuBtn} from '../../common/ShowMenuBtn/ShowMenuBtn';
 
 const StyledMenu = styled((props: MenuProps) => (
     <Menu
         elevation={0}
         anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'right',
+          vertical: 'bottom',
+          horizontal: 'right',
         }}
         transformOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
+          vertical: 'top',
+          horizontal: 'right',
         }}
         {...props}
     />
 ))(({theme}) => ({
-    '& .MuiPaper-root': {
-        marginTop: theme.spacing(1),
-        minWidth: 220,
-        backgroundColor: '#1e1e1f',
-        color: 'white',
-        '& .MuiMenu-list': {
-            padding: '4px 0',
-        },
-        '& .MuiMenu-button': {
-            backgroundColor: 'yellow',
-        },
-        '& .MuiMenuItem-root': {
-            '&:active': {
-                backgroundColor: '#1e1e1f',
-                maxWidth: 20,
-            },
-        },
+  '& .MuiPaper-root': {
+    marginTop: theme.spacing(1),
+    minWidth: 220,
+    backgroundColor: '#1e1e1f',
+    color: 'white',
+    '& .MuiMenu-list': {
+      padding: '4px 0',
     },
+    '& .MuiMenu-button': {
+      backgroundColor: 'yellow',
+    },
+    '& .MuiMenuItem-root': {
+      '&:active': {
+        backgroundColor: '#1e1e1f',
+        maxWidth: 20,
+      },
+    },
+  },
 }));
 
-export const HeaderMenu = () => {
-    const navigate = useNavigate();
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+interface Props {
+  className: string;
+}
 
-    const handleLogout = async (e: SyntheticEvent) => {
-        e.preventDefault();
-        try {
-            const data = await fetch('http://localhost:8080/login', {
-                method: 'DELETE',
-                credentials: 'include',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },
-            });
-            if (data.status === 200) {
-                navigate('/login');
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    };
+export const HeaderMenu = (props: Props) => {
+  const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-    return (
-        <div>
-            <button
-                onClick={handleClick}
-                className="show-menu-btn">
-                <ArrowDropDownIcon/>
-            </button>
-            <StyledMenu
-                id="demo-customized-menu"
-                MenuListProps={{
-                    'aria-labelledby': 'demo-customized-button',
-                }}
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}>
-                <MenuItem
-                    className="menu-item"
-                    onClick={handleClose}
-                    disableRipple>
-                    Konto
-                </MenuItem>
-                <MenuItem
-                    className="menu-item"
-                    onClick={handleLogout}
-                    disableRipple>
-                    Wyloguj
-                </MenuItem>
-                <link
-                    rel="stylesheet"
-                    href="/test"
-                />
-            </StyledMenu>
-        </div>
-    );
+  const handleLogout = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    try {
+      const data = await fetch('http://localhost:8080/login', {
+        method: 'DELETE',
+        credentials: 'include',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      });
+      if (data.status === 200) {
+        navigate('/login');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+      <div className={props.className}>
+        <ShowMenuBtn onClick={handleClick}/>
+        <StyledMenu
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}>
+          <MenuBtn
+              text="Konto"
+              to="/hr/profile"></MenuBtn>
+          <MenuBtn
+              text="Wyloguj"
+              onClick={handleLogout}></MenuBtn>
+        </StyledMenu>
+      </div>
+  );
 };
