0f359696bacbc110c413b2b1559b659189cf1994
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-07-31 21:06:16 +0200

    Moved mini logo to separate component.

---
+++ src/common/MicroLogoMegaK/MicroLogoMegaK.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MicroLogoMegaK.css';
+
+export const MicroLogoMegaK = () => {
+    return (
+        <div>
+            <img
+                src="../../logo-megak.webp"
+                alt="Logo MegaK"
+                className="micro-logo"
+            />
+        </div>
+    );
+};
