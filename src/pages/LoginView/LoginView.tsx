bdcfdab1044f3eab094f40d6c5dc5e6f800f0aa1
Patrycja Ińska <patrycja.inska@interia.pl>
2022-08-01 20:15:33 +0200

    Added navigation after login in system. Fixed bugs with fetch. Tested all forms.

--- src/pages/LoginView/LoginView.tsx
+++ src/pages/LoginView/LoginView.tsx
@@ -1,28 +1,6 @@
 import React from 'react';
-// import React, { useEffect, useState } from 'react';
-
 import { LoginBox } from '../../components/LoginBox/LoginBox';
-// import { useSelector } from 'react-redux';
-// import { StoreState } from '../../redux-toolkit/store';
-// import { useNavigate } from 'react-router-dom';
 
 export const LoginView = () => {
-  // const { role } = useSelector((store: StoreState) => store.user);
-  // let navigate = useNavigate();
-
-  // useEffect(() => {
-  //   switch (role){
-  //     case 'admin':
-  //       navigate('/home-admin');
-  //       break;
-  //     case 'hr':
-  //       navigate('/hr/home');
-  //       break;
-  //     case 'student':
-  //       navigate('/student');
-  //       break;
-  //   }
-  // });
-
   return <LoginBox />;
 };
