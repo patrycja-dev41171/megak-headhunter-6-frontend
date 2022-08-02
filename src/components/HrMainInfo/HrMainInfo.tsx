2df5486a2f40d1f389371c5eab15032953ab8d79
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-08-02 21:16:27 +0200

    Added simple structure to component.

--- src/components/HrMainInfo/HrMainInfo.tsx
+++ src/components/HrMainInfo/HrMainInfo.tsx
@@ -2,6 +2,25 @@ import React from 'react';
 
 import './HrMainInfo.css';
 
+function AnimatedSecondaryButton() {
+  return null;
+}
+
 export const HrMainInfo = () => {
-  return <div className="main"></div>;
+  return (
+    <div className="main">
+      <div className="info-header">Dane profilowe:</div>
+      <div className="info-body">Jan Kowalski</div>
+      <div className="info-header">Email</div>
+      <div className="info-body">jan.kowalski@gmail.com</div>
+      <div className="info-header">Firma</div>
+      <div className="info-body">MegaK Software House</div>
+      <div className="info-header">Studenci</div>
+      <div className="info-body">
+        <p className="student-reserved">Zarezerwowani: 3</p>
+        <p className="students-available">Dostępne miejsca: 2</p>
+      </div>
+      <AnimatedSecondaryButton />
+    </div>
+  );
 };
