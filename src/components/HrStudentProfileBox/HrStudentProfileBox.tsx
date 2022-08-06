2c99e6f996c5aa26a15c7d4eaf1d11a5677aee68
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-08-06 13:39:02 +0200

    Added HrStudentProfileBox component and styles.

---
+++ src/components/HrStudentProfileBox/HrStudentProfileBox.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './HrStudentProfileBox.css'
+
+export const HrStudentProfileBox = () => {
+    return (
+        <>
+            <h1>Hr Student Profile View</h1>
+            <p>Strona z widokiem cv kursanta. Makieta numer 6.</p>
+            <p>Dostęp: Zalogowany hr</p>
+        </>
+    );
+};
