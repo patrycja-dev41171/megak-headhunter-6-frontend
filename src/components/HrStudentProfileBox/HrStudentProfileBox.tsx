ae56037bc9062be36abdcac24d2b37a160a2a2f5
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-08-06 21:37:48 +0200

    Configured HrStudentProfile component.

--- src/components/HrStudentProfileBox/HrStudentProfileBox.tsx
+++ src/components/HrStudentProfileBox/HrStudentProfileBox.tsx
@@ -1,13 +1,75 @@
 import React from 'react';
-
-import './HrStudentProfileBox.css'
+import { Header } from '../Header/Header';
+import './HrStudentProfileBox.css';
+import { Box, Container } from '@mui/material';
+import { StudentExpectations } from '../StudentExpectations/StudentExpectations';
+import { SidebarHr } from '../Sidebars/SidebarHr';
 
 export const HrStudentProfileBox = () => {
-    return (
-        <>
-            <h1>Hr Student Profile View</h1>
-            <p>Strona z widokiem cv kursanta. Makieta numer 6.</p>
-            <p>Dostęp: Zalogowany hr</p>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <div className="main-container pageWithHeader">
+        <Container
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            marginTop: '26px',
+            '&.MuiContainer-root': {
+              maxWidth: '1522px',
+              position: 'relative',
+              left: '-70px',
+            },
+          }}>
+          <div className="hrStudentProfileView_button">dfs</div>
+          <div className="sidebarBox">
+            <SidebarHr
+              email="j.kowalski@op.pl"
+              fullName="Jan Kowalski"
+            />
+          </div>
+          <Box
+            sx={{
+              backgroundColor: '#222224',
+              width: '1176px',
+            }}>
+            <h3 className="hrStudentProfileView_subtitle">Oceny</h3>
+            <div className="hrStudentProfileView_line">
+              <div className="hrStudentProfileView_gradesContent displayCenter">Kontener z ocenami</div>
+            </div>
+            <h3 className="hrStudentProfileView_subtitle">Oczekiwanie w stosunku do zatrudnienia</h3>
+            <div className="hrStudentProfileView_line">
+              <div className="hrStudentProfileView_gradesContent displayCenter">
+                <StudentExpectations />
+              </div>
+            </div>
+            <h3 className="hrStudentProfileView_subtitle">Edukacja</h3>
+            <div className="hrStudentProfileView_line">
+              <div className="hrStudentProfileView_gradesContent displayCenter">Kontener z opisem edukacji</div>
+            </div>
+            <h3 className="hrStudentProfileView_subtitle">Kursy</h3>
+            <div className="hrStudentProfileView_line">
+              <div className="hrStudentProfileView_gradesContent displayCenter">Kontener z opisem kursów studenta</div>
+            </div>
+            <h3 className="hrStudentProfileView_subtitle">Doświadczenie zawodowe</h3>
+            <div className="hrStudentProfileView_line">
+              <div className="hrStudentProfileView_gradesContent displayCenter">Kontener z opisem doświadczenia zawodowego</div>
+            </div>
+            <h3 className="hrStudentProfileView_subtitle">Portfolio</h3>
+            <div className="hrStudentProfileView_line">
+              <div className="hrStudentProfileView_gradesContent displayCenter">Kontener z linkami do portfolio</div>
+            </div>
+            <h3 className="hrStudentProfileView_subtitle">Projekt w zespole Scrumowym</h3>
+            <div className="hrStudentProfileView_line">
+              <div className="hrStudentProfileView_gradesContent displayCenter">Kontener z linkami do projektu scrumowego</div>
+            </div>
+            <h3 className="hrStudentProfileView_subtitle">Projekt na zaliczenie</h3>
+            <div className="hrStudentProfileView_line">
+              <div className="hrStudentProfileView_gradesContent displayCenter">Kontener z linkami do projektu na zaliczenie</div>
+            </div>
+          </Box>
+        </Container>
+      </div>
+    </>
+  );
 };
