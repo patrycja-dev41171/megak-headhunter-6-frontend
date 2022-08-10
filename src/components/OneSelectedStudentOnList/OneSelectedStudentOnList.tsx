0d094a71a8117d372b81e3ca947371923283614c
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-08-10 20:59:27 +0200

    Refactoring component.

--- src/components/OneSelectedStudentOnList/OneSelectedStudentOnList.tsx
+++ src/components/OneSelectedStudentOnList/OneSelectedStudentOnList.tsx
@@ -3,27 +3,37 @@ import React from 'react';
 import './OneSelectedStudentOnList.css';
 import { MainButton } from '../../common/MainButton/MainButton';
 import { Avatar } from '@mui/material';
+import { ShowDetailsBtn } from '../../common/ShowDetailsBtn/ShowDetailsBtn';
 
 export const OneSelectedStudentOnList = () => {
   return (
     <div className="student_container">
-      <div className="reserved_box">Rezerwacja do</div>
-      <div className="student_info">
-        <div>
-          <Avatar
-            // alt={fullName}
-            // src={img_src ? img_src : imagePreview}
-            sx={{ width: 45, height: 45 }}
-          />
+      <div className="main_section">
+        <div className="reserved_box">
+          <div className="reserved_text">Rezerwacja do</div>
+          <div>2022.05.12 r.</div>
         </div>
-        <div>Imię i nazwisko</div>
+        <div className="student_info">
+          <div className="student_avatar">
+            <Avatar
+              // alt={fullName}
+              // src={img_src ? img_src : imagePreview}
+              sx={{ width: 45, height: 45 }}
+            />
+          </div>
+          <div>Jan Kowalski</div>
+        </div>
       </div>
-      <div className="buttons_box">
-        <MainButton>Pokaż CV</MainButton>
-        <MainButton>Brak zainteresowania</MainButton>
-        <MainButton>Zatrudniony</MainButton>
+      <div className="main_section">
+        <div className="buttons_box">
+          <MainButton>Pokaż CV</MainButton>
+          <MainButton>Brak zainteresowania</MainButton>
+          <MainButton>Zatrudniony</MainButton>
+        </div>
+        <div className="show_details_section">
+          <ShowDetailsBtn />
+        </div>
       </div>
-      <div className="show_details_btn">Rozwiń</div>
     </div>
   );
 };
