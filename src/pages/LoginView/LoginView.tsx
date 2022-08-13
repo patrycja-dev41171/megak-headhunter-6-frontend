521d3c308979a2f53568727669546c48e2a3dbc1
Patrycja Ińska <patrycja.inska@interia.pl>
2022-08-13 14:59:37 +0200

    Fixed some bugs.

--- src/pages/LoginView/LoginView.tsx
+++ src/pages/LoginView/LoginView.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { LoginBox } from '../../components/LoginBox/LoginBox';
-import {useSelector} from "react-redux";
-import {StoreState} from "../../redux-toolkit/store";
 
 export const LoginView = () => {
-  const { isLoggedIn } = useSelector((store: StoreState) => store.user);
-  return !isLoggedIn ?  null : <LoginBox />;
+    return <LoginBox />;
 };
